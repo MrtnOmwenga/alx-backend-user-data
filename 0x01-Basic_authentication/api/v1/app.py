add838aeb92eea040b19eab19f04ab7493cd11da
MrtnOmwenga <martin36449@gmail.com>
2023-08-08 23:04:00 +0300

    Task 5: Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -43,12 +43,11 @@ def not_found(error) -> str:
 @app_views.before_request
 def filter() -> None:
     """ Filter requests """
-    if auth is None:
+    if auth:
         excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
         if auth.require_auth(request.path, excluded_paths) is None:
             if auth.authorization_header(request) is None:
                 abort(401)
-
             if auth.current_user(request) is None:
                 abort(403)
 
