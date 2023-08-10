bd5ad5fca56b3d0777a6d489f5cdd841abc0a4a3
MrtnOmwenga <martin36449@gmail.com>
2023-08-10 23:32:56 +0300

    Task 100: Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Handling authentication """
+import re
 from flask import request
 from typing import List, TypeVar
 
@@ -9,14 +10,17 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Chacks if path requires authentication """
-        if path is None or excluded_paths is None:
-            return True
-
-        if path[-1] != '/':
-            path += '/'
-        if path in excluded_paths:
-            return False
-
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
